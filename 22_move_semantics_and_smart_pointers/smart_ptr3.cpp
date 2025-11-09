f578b71bcc9df20997339798bf6ad7d0c2073fe6
Joseph Bak <joseph.bak31415@gmail.com>
2025-11-08 22:25:47 -0500

    c++ move sementics and smart pointers

---
+++ 22_move_semantics_and_smart_pointers/smart_ptr3.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+template <typename T>
+class Auto_ptr2
+{
+	T* m_ptr {};
+public:
+	Auto_ptr2(T* ptr=nullptr)
+		:m_ptr(ptr)
+	{
+	}
+
+	~Auto_ptr2()
+	{
+		delete m_ptr;
+	}
+
+	// A copy constructor that implements move semantics
+	Auto_ptr2(Auto_ptr2& a) // note: not const
+	{
+		// We don't need to delete m_ptr here.  This constructor is only called when we're creating a new object, and m_ptr can't be set prior to this.
+		m_ptr = a.m_ptr; // transfer our dumb pointer from the source to our local object
+		a.m_ptr = nullptr; // make sure the source no longer owns the pointer
+		std::cout << "Copy constructor is invoked" << std::endl;
+	}
+
+	// An assignment operator that implements move semantics
+	Auto_ptr2& operator=(Auto_ptr2& a) // note: not const
+	{
+		if (&a == this)
+			return *this;
+
+		delete m_ptr; // make sure we deallocate any pointer the destination is already holding first
+		m_ptr = a.m_ptr; // then transfer our dumb pointer from the source to the local object
+		a.m_ptr = nullptr; // make sure the source no longer owns the pointer
+		std::cout << "Operator overload is invoked" << std::endl;
+
+		return *this;
+	}
+
+	T& operator*() const { return *m_ptr; }
+	T* operator->() const { return m_ptr; }
+	bool isNull() const { return m_ptr == nullptr; }
+};
+
+class Resource
+{
+public:
+	Resource() { std::cout << "Resource acquired\n"; }
+	~Resource() { std::cout << "Resource destroyed\n"; }
+};
+
+int main()
+{
+	Auto_ptr2<Resource> res1(new Resource());
+	Auto_ptr2<Resource> res2; // Start as nullptr
+
+	std::cout << "res1 is " << (res1.isNull() ? "null\n" : "not null\n");
+	std::cout << "res2 is " << (res2.isNull() ? "null\n" : "not null\n");
+
+	res2 = res1; // res2 assumes ownership, res1 is set to null
+	// Auto_ptr2<Resource> res2(res1); // Start as nullptr
+
+
+	std::cout << "Ownership transferred\n";
+
+	std::cout << "res1 is " << (res1.isNull() ? "null\n" : "not null\n");
+	std::cout << "res2 is " << (res2.isNull() ? "null\n" : "not null\n");
+
+	return 0;
+}
