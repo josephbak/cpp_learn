e6f2edc356e0fb631fa53cb0f8916d4f73b8de9f
Joseph Bak <joseph.bak31415@gmail.com>
2025-11-09 12:52:31 -0500

    simple c++ calculator program: stack datastructure using linked list implemented

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
-
+#include <string>
+#include "node.hpp"
+#include "stack.hpp"
 
 
 int main(){
 
-    int x;
-    int y;
+    Stack s{};
+    std::string exp{};
 
-    std::cout << "Please type an arithematic expression: " << std::endl;
-    
+    // std::cout << "Please type an arithematic expression: " << std::endl;
+    std::cout << "postfix expression: ";
+    std::getline(std::cin >> std::ws, exp);
+    // std::cout << exp;
 
+    for (char& c : exp){
+        if (isdigit(c)) {
+            s.push(c - '0');
+        } else if (c != ' '){
+            int right {s.pop()};
+            int left  {s.pop()};
+            switch (c) {
+                case '+':
+                    s.push(left + right);
+                    break;
+                case '-':
+                    s.push(left - right);
+                    break;
+                case '*':
+                    s.push(left * right);
+                    break;
+                case '/':
+                    s.push(left / right);
+                    break;
+                }
+        }
+    }
+
+
+    int ans {s.pop()};
+    std::cout << "The answer is: " << ans << std::endl;
 
     return 0;
 }
--- calculator/stack.cpp
+++ calculator/stack.cpp
@@ -1,8 +1,39 @@
 #include "stack.hpp"
+#include "node.hpp"
+#include <cstddef>
+#include <stdexcept>
 
-Stack::Stack(){};     // Constructor prototype
-Stack::~Stack()=default;    // Destructor prototype
-void Stack::push(int value);    // Prototype
-int Stack::pop();               // Prototype
-int Stack::peek() const;        // Prototype
-bool Stack::isEmpty() const;    // Prototype
+Stack::Stack() : top{nullptr} {}
+Stack::~Stack(){
+    while (top) {
+        Node* tmp = top;
+        top = top->getNext();
+        delete tmp;
+    }
+}
+void Stack::push(int value){
+    Node* new_node = new Node(value);
+    new_node->setNext(top);
+    top = new_node;
+}
+int Stack::pop(){
+    if (!top) {
+        // empty stack
+        throw std::runtime_error("stack underflow");
+    }
+    Node* tmp = top;
+    int data = tmp->getData();
+    top = tmp->getNext();
+    delete tmp;
+    return data;
+}
+int Stack::peek() const{
+    if (!top) {
+        // emptyy stack
+        throw std::runtime_error("stack underflow");
+    }
+    return top->getData();
+}
+bool Stack::isEmpty() const{
+    return top == nullptr;
+}
