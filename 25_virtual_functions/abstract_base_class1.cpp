8f36770117a0a2070ce488e79559f11d91601200
Joseph Bak <joseph.bak31415@gmail.com>
2025-11-27 21:21:34 -0500

    c++ abstract base class and dynamic casting

--- 25_virtual_functions/abstract_base_class1.cpp
+++ 25_virtual_functions/abstract_base_class1.cpp
@@ -43,6 +43,7 @@ public:
 int main()
 {
 
+    
     Dragonfly dfly{"Sally"};
     std::cout << dfly.getName() << " says " << dfly.speak() << '\n';
 
--- 25_virtual_functions/dynamic_casting.cpp
+++ 25_virtual_functions/dynamic_casting.cpp
@@ -46,8 +46,9 @@ int main()
 
     Derived* d{ dynamic_cast<Derived*>(b) }; // use dynamic cast to convert Base pointer into Derived pointer
 
-	if (d) // make sure d is non-null
-		std::cout << "The name of the Derived is: " << d->getName() << '\n';
+	std::cout << "The name of the Derived is: " << d->getName() << '\n';
+	// if (d) // make sure d is non-null
+	// 	std::cout << "The name of the Derived is: " << d->getName() << '\n';
 
 	delete b;
 
